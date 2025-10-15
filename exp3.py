7597cbbd2023fe35fef5f483d25851abbf772847
roshh4 <roshinidharmaraj@gmail.com>
2025-10-15 12:17:07 +0530

    azure function integraton pt6

---
+++ exp3.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 1. Load and preprocess the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+# 2. Build the CNN model
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2,2)))
+model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# 3. Compile and train the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=10, batch_size=64, validation_split=0.2)
+
+# 4. Make an interactive prediction
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+index = int(input("Enter an index (0 to 9999) for test image: "))
+
+# **FIXED: Indentation added to the if block**
+if index < 0 or index >= len(x_test):
+    print("Invalid index. Using index 0 by default.")
+    index = 0
+
+test_image = x_test[index]
+true_label = np.argmax(y_test[index])
+
+# Get the model's prediction
+prediction = model.predict(np.expand_dims(test_image, axis=0))
+predicted_label = np.argmax(prediction)
+
+# 5. Visualize the result
+plt.figure(figsize=(4, 4))
+resized_image = tf.image.resize(test_image, [128, 128])
+plt.imshow(resized_image)
+plt.axis('off')
+plt.title(f"Predicted: {class_names[predicted_label]}\nActual: {class_names[true_label]}")
+plt.show()
